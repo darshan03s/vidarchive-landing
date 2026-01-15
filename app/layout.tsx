613cb5d0b0fafc6cebe8581c9a71bb75dc861c3b
Darshan S <darshans2727@gmail.com>
2026-01-15 11:40:53 +0530

    refactor: add OpenGraph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,29 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: 'VidArchive',
-  description: 'A desktop utility application for Windows for downloading media'
+  description: 'A desktop utility application for Windows for downloading media',
+  openGraph: {
+    title: 'VidArchive',
+    description: 'A desktop utility application for Windows for downloading media',
+    url: process.env.NEXT_PUBLIC_APP_URL!,
+    siteName: 'VidArchive',
+    locale: 'en_US',
+    type: 'website',
+    images: [
+      {
+        url: `${process.env.NEXT_PUBLIC_APP_URL!}/api/og`,
+        width: 1200,
+        height: 630,
+        alt: 'VidArchive'
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'VidArchive',
+    description: 'A desktop utility application for Windows for downloading media',
+    images: `${process.env.NEXT_PUBLIC_APP_URL!}/api/og`
+  }
 };
 
 export default function RootLayout({
