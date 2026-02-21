bcdf9c5a9302402cf6c482080d380c283ed8aad3
Darshan S <darshans2727@gmail.com>
2026-02-21 13:55:52 +0530

    feat: add analytics component to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,6 +90,7 @@ export default function RootLayout({
           {children}
           <Particles className="absolute inset-0 z-0" quantity={100} ease={80} refresh />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
